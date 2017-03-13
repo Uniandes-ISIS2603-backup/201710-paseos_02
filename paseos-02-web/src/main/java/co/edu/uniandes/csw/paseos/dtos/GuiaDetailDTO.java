8a5338209ee7d5d5dafd41c2d600b677b62e21e4
mdr.leon10 <mdr.leon10@ISIS2603-0197.sis.virtual.uniandes.edu.co>
2017-03-13 08:50:21 -0500

    Correccion

--- paseos-02-web/src/main/java/co/edu/uniandes/csw/paseos/dtos/GuiaDetailDTO.java
+++ paseos-02-web/src/main/java/co/edu/uniandes/csw/paseos/dtos/GuiaDetailDTO.java
@@ -7,7 +7,6 @@ package co.edu.uniandes.csw.paseos.dtos;
 
 import co.edu.uniandes.csw.paseos.entities.CalificacionEntity;
 import co.edu.uniandes.csw.paseos.entities.GuiaEntity;
-import co.edu.uniandes.csw.paseos.entities.PaseoEcologicoEntity;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -19,7 +18,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class GuiaDetailDTO extends GuiaDTO
 {
-    private List<PaseoEcologicoDTO> paseos;
+    private List<GuiaDTO> guias;
     
     //private List<CalificacionDTO> calificaciones;
     
@@ -31,30 +30,7 @@ public class GuiaDetailDTO extends GuiaDTO
     public GuiaDetailDTO(GuiaEntity entity)
     {
         super(entity);
-        if(entity != null)
-        {
-            if(entity.getPaseosEcologico() != null)
-            {
-                paseos = new ArrayList<PaseoEcologicoDTO>();
-                for (PaseoEcologicoEntity paseo : entity.getPaseosEcologico()) 
-                {
-                    paseos.add(new PaseoEcologicoDTO(paseo));
-                }
-                
-            }
-            
-            /*
-            if(entity.getCalificaciones( ) != null)
-            {
-                calificaciones = new ArrayList<CalificacionDTO>();
-                for (CalificacionEntity calificacion : entity.getCalificaciones())
-                {
-                    calificaciones.add(new CalificacionDTO(calificacion));
-                }
-            }
-           
-            */
-        }
+        //TODO: preguntarle a Juan David
     }
     
     @Override
@@ -62,7 +38,8 @@ public class GuiaDetailDTO extends GuiaDTO
     {
         GuiaEntity entity = super.toEntity();
         
-        if(this.getPaseos() != null)
+        //TODO: preguntar
+        /*if(this.getPaseos() != null)
         {            
             List<PaseoEcologicoEntity> paseosE = new ArrayList<PaseoEcologicoEntity>( );
             for(PaseoEcologicoDTO paseo : this.getPaseos())
@@ -71,7 +48,7 @@ public class GuiaDetailDTO extends GuiaDTO
             }
             entity.setPaseosEcologico(paseosE);
         }
-        
+        */
         /*
         if(this.getCalificaciones( ) != null)
         {
@@ -87,12 +64,12 @@ public class GuiaDetailDTO extends GuiaDTO
         return entity;
     }
 
-    public List<PaseoEcologicoDTO> getPaseos() {
-        return paseos;
+    public List<GuiaDTO> getGuias() {
+        return guias;
     }
 
-    public void setPaseos(List<PaseoEcologicoDTO> paseos) {
-        this.paseos = paseos;
+    public void setGuias(List<GuiaDTO> guias) {
+        this.guias = guias;
     }
 
     /*
