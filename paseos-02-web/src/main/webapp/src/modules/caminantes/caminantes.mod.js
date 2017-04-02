cc2d26760a0cb12dd903ea51b2c8cb0caed5e1ed
jd.vega11 <jd.vega11@ISIS2603-0179.sis.virtual.uniandes.edu.co>
2017-04-02 11:32:26 -0500

    Caminantes (web) list and detail

--- paseos-02-web/src/main/webapp/src/modules/caminantes/caminantes.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/caminantes/caminantes.mod.js
@@ -1,32 +1,53 @@
 (function (ng) {
-    // Definición del módulo
     var mod = ng.module("caminanteModule", ['ui.router']);
- 
-   // Configuración de los estados del módulo
+   
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            // En basePath se encuentran los templates y controladores de módulo
             var basePath = 'src/modules/caminantes/';
-            // Mostrar la lista de caminantes será el estado por defecto del módulo
             $urlRouterProvider.otherwise("/caminantesList");
-            // Definición del estado 'caminantesList' donde se listan los caminantes
-            $stateProvider.state('caminantesList', {
-                // Url que aparecerá en el browser
-                url: '/caminantes/list',
-                // Se define una variable books (del estado) que toma por valor 
-                // la colección de libros que obtiene utilizando $http.get 
-                 resolve: {
+
+            $stateProvider.state('caminantes', {
+                url: '/caminantes',
+                abstract: true,
+                resolve: {
                     caminantes: ['$http', function ($http) {
-                            return $http.get('data/caminantes.json'); // $http retorna una promesa que aquí no se está manejando si viene con error.
+                            return $http.get('data/caminantes.json');
                         }]
                 },
-                // Template que se utilizara para ejecutar el estado
-                templateUrl: basePath + 'caminantes.list.html',
-                // El controlador guarda en el scope en la variable caminantesRecords los datos que trajo el resolve
-                // caminantesRecords será visible en el template
-                controller: ['$scope', 'caminantes', function ($scope, caminantes) {
-                        $scope.caminantesRecords = caminantes.data;
-                    }]              
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'caminantes.html',
+                        controller: ['$scope', 'caminantes', function ($scope, caminantes) {
+                                $scope.caminantesRecords = caminantes.data;
+                            }]
+                    }
+                }
+            }).state('caminantesList', {
+                url: '/list',
+                parent: 'caminantes',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'caminantes.list.html'
+                    }
+                }
+            }).state('caminanteDetail', {
+                url: '/{caminanteId:int}/detail',
+                parent: 'caminantes',
+                param: {
+                    caminanteId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'caminantes.list.html'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'caminantes.detail.html',
+                        controller: ['$scope', '$stateParams', function ($scope, $params) {
+                                $scope.currentCaminante = $scope.caminantesRecords[$params.caminanteId-1];
+                            }]
+                    }
+
+                }
+
             });
-        }
-    ]);
+        }]);
 })(window.angular);
