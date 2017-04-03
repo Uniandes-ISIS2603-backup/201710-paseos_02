f9eca5a2982126501bfb3c09f8d5bb6e611e9e1f
Maria del Rosario León <mdr.leon10@uniandes.edu.co>
2017-04-03 08:09:41 -0500

    guias

--- paseos-02-web/src/main/webapp/src/modules/guias/guias.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/guias/guias.mod.js
@@ -1,7 +1,55 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function (ng) {
+    var mod = ng.module('guiaModule', ['ui.router'])
+    mod.constant('guiasContext', 'api/guias')
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        var basePath = 'src/modules/guias/'
+        $urlRouterProvider.otherwise('/guiasList')
+
+        $stateProvider
+            .state('guias', {
+                url: '/guias',
+                abstract: true,
+                resolve: {
+                    guias: ['$http', function ($http) {
+                        //  return $http.get( guiasContext )
+                        return $http.get('data/guias.json')
+                    }]
+                },
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'guias.html',
+                        controller: ['$scope', 'guias', function ($scope, guias) {
+                            $scope.guiasRecords = guias.data
+                        }]
+                    }
+                }
+            })
+            .state('guiasList', {
+                url: '/list',
+                parent: 'guias',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'guias.list.html'
+                    }
+                }
+            })
+            .state('guiaDetail', {
+                url: '/{guiaId:int}/detail',
+                parent: 'guias',
+                param: {
+                    guiaId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + 'guias.detail.html',
+                        controller: ['$scope', '$stateParams', function ($scope, $params) {
+                            $scope.currentGuia = $scope.guiasRecords[$params.guiaId - 1]
+                        }]
+                    }
+                }
+            })
+    }])
+})(window.angular)
+
 
 
