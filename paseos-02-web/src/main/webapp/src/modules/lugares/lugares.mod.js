10111d95a7a804e88af1ead2beb1d955952b9f52
ac.lopez <ac.lopez@ISIS2603-0160.sis.virtual.uniandes.edu.co>
2017-04-04 00:01:12 -0500

    Lugar parte

--- paseos-02-web/src/main/webapp/src/modules/lugares/lugares.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/lugares/lugares.mod.js
@@ -3,5 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+(function (ng) {
+    var mod = ng.module("lugaresModule", ['ui.router']);
+   
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/lugares/';
+            $urlRouterProvider.otherwise("/lugares");
+
+            $stateProvider.state('lugares', {
+                url: '/lugares',
+                abstract: true,
+                resolve: {
+                    lugares: ['$http', function ($http) {
+                            return $http.get('data/lugares.json');
+                        }]
+                },
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'lugares.html',
+                        controller: ['$scope', 'lugares', function ($scope, lugares) {
+                                $scope.lugaresRecords = lugares.data;
+                            }]
+                    }
+                }
+            }).state('lugaresList', {
+                url: '/list',
+                parent: 'lugares',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'lugares.list.html'
+                    }
+                }
+            }).state('lugaresDetail', {
+                url: '/{lugaresId:int}/detail',
+                parent: 'lugares',
+                param: {
+                    lugaresId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'lugares.list.html'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'lugares.detail.html',
+                        controller: ['$scope', '$stateParams', function ($scope, $params) {
+                                $scope.currentlugares = $scope.lugaresRecords[$params.lugaresId-1];
+                            }]
+                    }
+
+                }
+
+            });
+        }]);
+})(window.angular);
 
 
