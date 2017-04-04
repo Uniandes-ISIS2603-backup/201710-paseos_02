747e8b7eb039cdfbc4bcf13da0566e9401fa92e0
jd.chaves <jd.chaves@ISIS2603-0168.sis.virtual.uniandes.edu.co>
2017-04-03 23:42:18 -0500

    Cosas de actividades list y mod

--- paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
@@ -1,33 +1,39 @@
 (function (ng) {
-    // Definición del módulo
     var mod = ng.module("actividadModule", ['ui.router']);
- 
-   // Configuración de los estados del módulo
+
+    mod.constant("paseosContext", "api/paseos");
+
+    mod.constant("actividadesContext", "actividades");
+
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            // En basePath se encuentran los templates y controladores de módulo
-            var basePath = 'src/modules/actividades/';
-            // Mostrar la lista de caminantes será el estado por defecto del módulo
+            var basePath = 'src/modules/actuvidades/';
             $urlRouterProvider.otherwise("/actividadesList");
-           
-            $stateProvider.state('actividadesList', {
-                // Url que aparecerá en el browser
-                url: '/actividades/list',
-                // Se define una variable books (del estado) que toma por valor 
-                // la colección de libros que obtiene utilizando $http.get 
-                 resolve: {
-                    actividades: ['$http', function ($http) {
-                            return $http.get('data/actividades.json'); // $http retorna una promesa que aquí no se está manejando si viene con error.
+
+            $stateProvider.state('actividades', {
+                url: '/actividades',
+                abstract: true,
+                parent: 'paseoDetail',
+                resolve: {
+                    reviews: ['$http', 'paseosContext', 'actividadesContext', '$stateParams', function ($http, paseosContext, actividadesContext, $params) {
+                            return $http.get(paseosContext + '/' + $params.paseoId + '/' + actividadesContext);
                         }]
                 },
-                // Template que se utilizara para ejecutar el estado
-                templateUrl: basePath + 'actividades.list.html',
-                // El controlador guarda en el scope en la variable paseosEcologicosRecords los datos que trajo el resolve
-                // paseosEcologicosRecords será visible en el template
-                controller: ['$scope', 'actividades', function ($scope, paseosEcologicos) {
-                        $scope.actividadesRecords = paseosEcologicos.data;
-                    }]              
+                views: {
+                    'childrenView': {
+                        templateUrl: basePath + 'actividades.html'
+                    }
+                },
+            }).state('actividadesList', {
+                url: '/list',
+                parent: 'actividades',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'actvidades.list.html',
+                        controller: ['$scope', 'actividades', function ($scope, actividades) {
+                                $scope.actividadesRecords = actividades.data;
+                            }]
+                    }
+                }
             });
-        }
-    ]);
+        }]);
 })(window.angular);
-
