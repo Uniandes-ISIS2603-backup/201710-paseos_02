9a914cf5ea54d516319a2b0be84f16e44a8112cf
jd.chaves <jd.chaves@ISIS2603-0168.sis.virtual.uniandes.edu.co>
2017-04-03 22:50:20 -0500

    Cosas de actividades

--- paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
@@ -1,7 +1,33 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+(function (ng) {
+    // Definición del módulo
+    var mod = ng.module("actividadModule", ['ui.router']);
+ 
+   // Configuración de los estados del módulo
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            // En basePath se encuentran los templates y controladores de módulo
+            var basePath = 'src/modules/actividades/';
+            // Mostrar la lista de caminantes será el estado por defecto del módulo
+            $urlRouterProvider.otherwise("/actividadesList");
+           
+            $stateProvider.state('actividadesList', {
+                // Url que aparecerá en el browser
+                url: '/actividades/list',
+                // Se define una variable books (del estado) que toma por valor 
+                // la colección de libros que obtiene utilizando $http.get 
+                 resolve: {
+                    actividades: ['$http', function ($http) {
+                            return $http.get('data/actividades.json'); // $http retorna una promesa que aquí no se está manejando si viene con error.
+                        }]
+                },
+                // Template que se utilizara para ejecutar el estado
+                templateUrl: basePath + 'actividades.list.html',
+                // El controlador guarda en el scope en la variable paseosEcologicosRecords los datos que trajo el resolve
+                // paseosEcologicosRecords será visible en el template
+                controller: ['$scope', 'actividades', function ($scope, paseosEcologicos) {
+                        $scope.actividadesRecords = paseosEcologicos.data;
+                    }]              
+            });
+        }
+    ]);
+})(window.angular);
 
