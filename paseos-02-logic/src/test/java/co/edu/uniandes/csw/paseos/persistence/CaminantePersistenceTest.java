e18e18b9d85201ffa699883f78e5563467c13b74
jd.vega11 <jd.vega11@ISIS2603-0179.sis.virtual.uniandes.edu.co>
2017-04-27 19:08:08 -0500

    CaminantePersistenceTest - finalizado

--- paseos-02-logic/src/test/java/co/edu/uniandes/csw/paseos/persistence/CaminantePersistenceTest.java
+++ paseos-02-logic/src/test/java/co/edu/uniandes/csw/paseos/persistence/CaminantePersistenceTest.java
@@ -125,8 +125,121 @@ public class CaminantePersistenceTest
         
         CaminanteEntity entityEncontrado = em.find(CaminanteEntity.class, entityPersistido.getId());
         Assert.assertNotNull("El caminante deberia existir en la base de datos",entityEncontrado);
-        Assert.assertEquals(entityParaPrueba.getNombre(), entityEncontrado.getNombre());
+        
+        //Se verifica que los valores persistidos sean correctos
+        verificarConsistenciaAtributos(entityParaPrueba, entityEncontrado);
     }
-
+    
+    @Test
+    public void getCaminantesTest( )
+    {
+        List<CaminanteEntity> encontrados = caminantePersistence.findAll();
+        Assert.assertEquals(data.size(), encontrados.size());
+        boolean found;
+        for(CaminanteEntity encontrado : encontrados)
+        {
+            found = false;
+            for(CaminanteEntity esperado : data)
+            {
+                if(encontrado.getId().equals(esperado.getId()))
+                {
+                    verificarConsistenciaAtributos(esperado, encontrado);
+                    found = true;
+                    break;
+                }
+            }
+            Assert.assertTrue(found);
+        }        
+    }
+    
+    //Escenario 1: El caminante buscado existe en la base de datos
+    @Test
+    public void getCaminanteTest1( )
+    {
+        CaminanteEntity esperado = data.get(0);
+        CaminanteEntity encontrado = caminantePersistence.find(esperado.getId());
+        Assert.assertNotNull(encontrado);
+        verificarConsistenciaAtributos(esperado, encontrado);
+    }
+    
+    //Escenario 2: No existe un caminante con el id dado 
+    @Test
+    public void getCaminanteTest2( )
+    {
+        CaminanteEntity respuesta = caminantePersistence.find(Long.MAX_VALUE);
+        Assert.assertNull(respuesta);      
+    }
+    
+    //Escenario 1: Se va a actualizar un caminante que existe en la base de datos.
+    @Test 
+    public void updateCaminanteTest1( )
+    {
+        CaminanteEntity original = data.get(0);
+        
+        PodamFactory podam = new PodamFactoryImpl();        
+        CaminanteEntity actualizada = podam.manufacturePojo(CaminanteEntity.class);
+        actualizada.setId(original.getId());
+        
+        CaminanteEntity mergeResult = caminantePersistence.update(actualizada);
+        Assert.assertNotNull(mergeResult);
+        
+        CaminanteEntity encontrada = em.find(CaminanteEntity.class, original.getId());
+        Assert.assertNotNull("El caminante se elimino en lugar de actualizarse", encontrada);
+        verificarConsistenciaAtributos(actualizada, encontrada);
+    }
+    
+    //Escenario 2: Se va a actualizar un caminante que no existe en la base de datos.
+    @Test 
+    public void updateCaminanteTest2( )
+    {      
+        PodamFactory podam = new PodamFactoryImpl();        
+        CaminanteEntity newEntity = podam.manufacturePojo(CaminanteEntity.class);
+        newEntity.setId(Long.MAX_VALUE);
+        
+        CaminanteEntity mergeResult = caminantePersistence.update(newEntity);
+        
+        //Como no hay un caminante con el mismo id en la BD, al hacer merge el newEntity se debe persistir.
+       //No obstante, el newEntity no se persiste con el id que se puso manualmente, puesto que se definio que los id's son generados por la BD.
+       //Vale la pena aclarar que el newEntity no se sincroniza con la BD, por tanto su id no es el generado automaticamente.
+       
+       
+       //Por lo anterior, si se busca un caminante con el id que se puso manualmente, no se encuentran resultados.
+        CaminanteEntity rtaIdOriginal = em.find(CaminanteEntity.class, newEntity.getId());
+        Assert.assertNull(rtaIdOriginal);
+        
+        //Sin embargo, al buscar con el id que genero la base de datos, se encuentra el caminante efectivamente persistido.
+        CaminanteEntity rtaIdGenerado = em.find(CaminanteEntity.class, mergeResult.getId());
+        Assert.assertNotNull("Debio persistirse el caminante en la base de datos", rtaIdGenerado);
+        verificarConsistenciaAtributos(newEntity, rtaIdGenerado);
+    }
+    
+    @Test
+    public void deleteCaminanteTest( )
+    {
+        CaminanteEntity entity = data.get(0);
+        caminantePersistence.delete(entity.getId());
+        CaminanteEntity eliminado = em.find(CaminanteEntity.class, entity.getId());
+        Assert.assertNull(eliminado);
+    }
+    
+    private void verificarConsistenciaAtributos(CaminanteEntity c1, CaminanteEntity c2)
+    {
+        Assert.assertEquals(c1.getNombre(), c2.getNombre());
+        Assert.assertEquals(c1.getIdentificacion(), c2.getIdentificacion());
+        Assert.assertEquals(c1.getTipoIdentificacion(), c2.getTipoIdentificacion());
+        Assert.assertEquals(c1.getEdad(), c2.getEdad());
+        Assert.assertEquals(c1.getDireccion(), c2.getDireccion());
+        Assert.assertEquals(c1.getTelefono(), c2.getTelefono());
+        Assert.assertEquals(c1.getCorreoElectronico(), c2.getCorreoElectronico());
+        Assert.assertEquals(c1.getImagen(), c2.getImagen()); 
+       
+        List<Integer> condFisicasOriginal = c1.getCondicionesFisicas();
+        List<Integer> condfisicasEncontrado = c2.getCondicionesFisicas();
+        
+        for(Integer condFisica : condfisicasEncontrado)
+        {
+            Assert.assertTrue(condFisicasOriginal.contains(condFisica));
+        }       
+    }
    
 }
