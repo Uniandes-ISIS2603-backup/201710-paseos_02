3c6ccb262a66e6d97d44c3f84f017b7b871f6b9d
David Narvaez <d.narvaez11@uniandes.edu.co>
2017-05-16 21:06:15 -0500

    ya funcionan los servicios !!!!!!!

--- paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
+++ paseos-02-web/src/main/webapp/src/modules/actividades/actividades.mod.js
@@ -29,8 +29,87 @@
                 views: {
                     'listView': {
                         templateUrl: basePath + 'actividades.list.html',
-                        controller: ['$scope', 'actividades', function ($scope, actividades) {
+                        controller: ['$scope', 'actividades','$http', function ($scope, actividades, $http) {
                                 $scope.actividadesRecords = actividades.data;
+                                
+                                
+                                $scope.borrarActividad = function(index){
+                                    var act = $scope.actividadesRecords[index];
+                                    $http.delete('api/paseos/'+$scope.currentPaseo.id +'/actividades/'+act.id).
+                                            then(function(response){
+                                                $scope.actividadesRecords.splice(index,1);
+                                    })
+                                    
+                                }
+                                
+                            }]
+                    }
+                }
+            }).state('actividadesFormulario', {
+                url: '/formulario',
+                parent: 'actividades',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'actividades.formulario.html',
+                        controller: ['$http','$scope',
+                            function ($http,$scope) {
+                                                         
+                                $scope.agregarActividad = function(){
+                                    
+                                    console.log('mae');
+                                    
+                                    var nombreA = document.getElementById('nombreActividad').value;
+                                    var descripcionA = document.getElementById('descripcionActividad').value;
+                                    var duracionA = document.getElementById('duracionActividad').value;
+                                    var equipamientoA = document.getElementById('equipamientoActividad').value;
+                                    var objActividad = {
+                                        nombre: nombreA,
+                                        descripcion: descripcionA,
+                                        duracion: duracionA,
+                                        equipamiento: equipamientoA.split(","),
+                                        paseoEcologico:{
+                                            id: $scope.currentPaseo.id
+                                        }
+                                    };
+                                    $http.post('api/paseos/'+$scope.currentPaseo.id +'/actividades', objActividad);
+                                };
+                            }]
+                    }
+                }
+            }).state('actividadesFormularioPut', {
+                url: '/{actividadId:int}/formulario',
+                parent: 'actividades',
+                 param: {
+                    actividadId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'actividades.formulario.put.html',
+                        controller: ['$http','$scope', '$stateParams','$state',
+                            function ($http,$scope, $params,$state) {
+                                                         
+                                $scope.actualizarActividad = function(){
+                                    
+                                    var nombreA = document.getElementById('nombreActividadPut').value;
+                                    var descripcionA = document.getElementById('descripcionActividadPut').value;
+                                    var duracionA = document.getElementById('duracionActividadPut').value;
+                                    var equipamientoA = document.getElementById('equipamientoActividadPut').value;
+                                    var objActividad = {
+                                        nombre: nombreA,
+                                        descripcion: descripcionA,
+                                        duracion: duracionA,
+                                        equipamiento: equipamientoA.split(","),
+                                        paseoEcologico:{
+                                            id: $scope.currentPaseo.id
+                                        }
+                                    };
+                                    
+                                    $http.put('api/paseos/'+$scope.currentPaseo.id +'/actividades/'+$params.actividadId, objActividad);
+                                    $state.go('actividadesList',{},{reload:true});
+                                    
+                                };
+                                
+                                
                             }]
                     }
                 }
